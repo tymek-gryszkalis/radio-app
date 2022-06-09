754174946d92b706d4518b0cafcc984966ef3188
Sudowiec <lalarsek@gmail.com>
2022-06-09 20:22:34 +0200

    advanced pdf generating

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from pylatex import Document, Section, Subsection, Tabular
-from pylatex import Math, TikZ, Axis, Plot, Figure, Matrix, Alignat
-from pylatex.utils import italic
-import os
+from pylatex import Document, PageStyle, Head, MiniPage, Foot, LargeText, Figure, LineBreak, Description, simple_page_number, Command
+from pylatex.utils import bold, italic
 
 def getInput():
     heads = []
@@ -24,35 +22,63 @@ def getInput():
     main_hash = {"headers" : heads, "news" : news, "author" : author, "time" : hour, "pronouns" : pronouns}
 
 def genPdf(main_hash):
+    # pick right pronouns
     pn = ""
     if main_hash["pronouns"] == "f":
         pn = "a"
     elif main_hash["pronouns"] == "o":
         pn = "x"
 
+    # prepare strings
+    title = "Wiadomości studenckie " + main_hash["date"]
     intro = "Minęła godzina " + main_hash["time"] + " więc czas na wiadomości studenckie, a w nich:\n\n"
     outro = "\nA wiadomości przygotował" + pn + " " + main_hash["author"] + "."
 
+    # main latex code generating
     if __name__ == '__main__':
-        geometry = {"tmargin" : "1cm", "lmargin" : "1cm", "bmargin" : "1cm", "rmargin" : "1cm"}
+        # creating document
+        geometry = {"margin" : "1in"}
         doc = Document(geometry_options = geometry)
+        Command('documentclass', '10pt', 'doc')
 
-        doc.append(intro)
+        # creating header (for logo purposes only)
+        header = PageStyle("header")
+        with header.create(Head("R")):
+            header.append(bold("Radio Aktywne"))
+        doc.preamble.append(header)
+        doc.change_document_style("header")
+
+        # centralized minipage for title
+        with doc.create(MiniPage(align='c')):
+            doc.append(LargeText(bold(title)))
+
+        # generate headers
+        for i in range(5):
+            doc.append(LineBreak())
+        doc.append(italic(intro))
         for i in range(len(main_hash["headers"])):
-            doc.append(main_hash["headers"][i] + "\n")
-        doc.append("\nWiadomości przedstawi:\n\n")
-        for i in range(len(main_hash["news"])):
-            doc.append(main_hash["news"][i] + "\n")
-        doc.append(outro)
+            doc.append(bold(str(i + 1) + ". " + main_hash["headers"][i] + "\n"))
+        doc.append(italic("\nWiadomości przedstawi: [Osoba]"))
+
+        # generate news
+        with doc.create(Description()) as desc:
+            for i in range(len(main_hash["news"])):
+                desc.add_item(str(i + 1) + ".", Command('texttt', (main_hash["news"][i])))
+
+        doc.append(italic(outro))
 
         doc.generate_pdf('full', clean_tex=False)
 
 testhash = {
-    "headers" : ["header a", "header b", "header c"],
-    "news" : ["news one abcdefghijklmnopqrstuvwxyz", "news two abcdefghijklmnopqrstuvwxyz", "news three abcdefghijklmnopqrstuvwxyz"],
+    "headers" : ["Nagłówek A", "Nagłówek B", "Nagłówek C"],
+    "news" : [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sollicitudin mattis magna. In et luctus quam. Morbi scelerisque eu massa in sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean nec porttitor felis. Mauris sit amet purus est. Curabitur augue sapien, mollis vel risus venenatis, egestas tristique urna. Vestibulum tristique sollicitudin mi ut luctus.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sollicitudin mattis magna. In et luctus quam. Morbi scelerisque eu massa in sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean nec porttitor felis. Mauris sit amet purus est. Curabitur augue sapien, mollis vel risus venenatis, egestas tristique urna. Vestibulum tristique sollicitudin mi ut luctus.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sollicitudin mattis magna. In et luctus quam. Morbi scelerisque eu massa in sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean nec porttitor felis. Mauris sit amet purus est. Curabitur augue sapien, mollis vel risus venenatis, egestas tristique urna. Vestibulum tristique sollicitudin mi ut luctus."],
     "author" : "Tymi Gryszkalis",
     "time" : "19:30",
-    "pronouns" : "m"
+    "pronouns" : "m",
+    "date" : "04.20"
 }
 
 genPdf(testhash)
