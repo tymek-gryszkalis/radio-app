af7c4cfd333a1b798918c20770a048ee1bc3731d
Sudowiec <lalarsek@gmail.com>
2022-06-09 23:27:39 +0200

    menu choice

--- app.py
+++ app.py
@@ -1,23 +1,50 @@
 from tkinter import *
 
+option = "n"
+
+def selectOption(choice):
+    if choice == "Wiadomości Studenckie":
+        option = "n"
+        newsField()
+    elif choice == "Kurier Kulturalny":
+        option = "c"
+        notNewsField()
+    elif choice == "Wiadomości Sportowe":
+        option = "s"
+        notNewsField()
+
+def newsField():
+    for widget in frame.winfo_children():
+            widget.destroy()
+    for i in range(3):
+        inp_title = Entry(frame, width = "10", font = "Arial 12")
+        inp_news = Text(frame, width = "100", height = "5", font = "Arial 12")
+        inp_title.pack()
+        inp_news.pack()
+
+def notNewsField():
+    for widget in frame.winfo_children():
+            widget.destroy()
+    for i in range(3):
+        inp_news = Text(frame, width = "100", height = "5", font = "Arial 12")
+        inp_news.pack()
+
 root = Tk()
 root.geometry("1280x720")
-root.iconbitmap("icon.ico")
+root.iconbitmap('icon.ico')
 
 header = Label(root, text = "Generator newsów", font = "Arial 12 bold")
 header.pack()
-check_st = Checkbutton(root, text = "Studenckie")
-check_sp = Checkbutton(root, text = "Sportowe")
-check_k = Checkbutton(root, text = "Kurier")
-check_st.pack()
-check_sp.pack()
-check_k.pack()
-for i in range(3):
-    inp_title = Entry(root, width = "10", font = "Arial 12")
-    inp_news = Text(root, width = "100", height = "5", font = "Arial 12")
-    inp_title.pack()
-    inp_news.pack()
-but_generate = Button(root, text = "Generuj")
-but_generate.pack()
+
+news_options = ["Wiadomości Studenckie", "Kurier Kulturalny", "Wiadomości Sportowe"]
+options_var = StringVar()
+options_var.set(news_options[0])
+options_widget = OptionMenu(root, options_var, *news_options, command = selectOption)
+options_widget.pack()
+
+frame = Frame(root)
+frame.pack()
+
+newsField()
 
 root.mainloop()
